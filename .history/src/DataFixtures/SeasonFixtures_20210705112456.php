1ea098f100e4e7dd362bdda416bd4079244fe9f5
fourneau <cathyjulien14@orange.fr>
2021-07-05 11:41:25 +0200

    après load rrrr

---
+++ .history/src/DataFixtures/SeasonFixtures_20210705112456.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Episode;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class EpisodeFixtures extends Fixture implements DependentFixtureInterface
+{
+    const EPISODES = [
+        'episode1',
+        'episode2',
+        'episode3',
+        'episode4',
+        'episode5',
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::EPISODES as $key => $episodes) {
+            $episode = new Episode();
+            $episode->setTitle($episodes);;
+            $episode->setSeason($this->getReference('season_0'));
+            $episode->setNumber($key);
+            $episode->setSynopsis('Synopsis Des zombies envahissent la terre');
+            $manager->persist($episode);
+            $this->addReference('episode_' . $key, $episode);
+        }  
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+          SeasonFixtures::class,
+        ];
+    }
+}
---
+++ src/DataFixtures/SeasonFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Season;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class SeasonFixtures extends Fixture implements DependentFixtureInterface
+{
+    const SEASONS = [
+        'season1',
+        'season2',
+        'season3',
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::SEASONS as $key => $seasons) {
+            $season = new Season();
+            $season->setPrograms($this->getReference('program_0'));
+            $season->setNumber(1);
+            $season->setYear(2000);
+            $season->setDescription('Description Des zombies envahissent la terre');
+            $manager->persist($season);
+            $this->addReference('season_' . $key, $season);
+        }  
+        
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+          ProgramFixtures::class,
+        ];
+    }
+
+}
